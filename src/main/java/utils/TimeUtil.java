0ac55c03fabfccec5b0db44eb2e836ccfb7f974f
Ahmed Salama <ahmed.salama@ndgit.com>
2023-06-28 11:05:05 +0200

    annotate  TimeUtil.call with @UtilityClass

--- src/main/java/utils/TimeUtil.java
+++ src/main/java/utils/TimeUtil.java
@@ -2,11 +2,14 @@ package utils;
 
 import java.time.ZoneId;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class TimeUtil {
 
     public static final String TIMEZONE_BERLIN_STRING = "Europe/Berlin";
-    public static final ZoneId TIMEZONE_BERLIN = ZoneId.of("Europe/Berlin");
+
+    public static final ZoneId TIMEZONE_BERLIN = ZoneId.of(TIMEZONE_BERLIN_STRING);
 
     public static final String DATE_TIME_PATTERN = "dd-MM-yyyy HH:mm:ss";
-
 }
